f90a84002cce28c1bf0a07d0c46e5b9b4c9c9e09
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-08-10 01:20:48 +0800

    Update ..\Scripts
    GetExp

---
+++ Assets/galaxy890624/Datas/GetAward.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Object Data", menuName = "Object Data/new Award Data")]
+public class GetAward : ScriptableObject
+{
+    public float GetExp = 0;
+}
--- Assets/galaxy890624/Scripts/GetExp.cs
+++ Assets/galaxy890624/Scripts/GetExp.cs
@@ -1,15 +1,23 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GetExp : MonoBehaviour
 {
+    public GameObject Exp;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            
+        }
+    }
     // Update is called once per frame
     void Update()
     {
