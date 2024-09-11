22374efe901b9d12ed2d6c1095b16af7bef58b6c
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-09-11 21:29:58 +0800

    Update IsGround.cs

--- Assets/galaxy890624/Scripts/IsGround.cs
+++ Assets/galaxy890624/Scripts/IsGround.cs
@@ -8,6 +8,24 @@ using UnityEngine;
 public class IsGround : MonoBehaviour
 {
     public bool Grounded = false; // initialize
+    [SerializeField] float DetectRadius = 0.2f;
+    [SerializeField] LayerMask GroundLayer; // 地板的圖層
+
+    private void FixedUpdate()
+    {
+        // 用物理引擎瞬間畫一個球 並且捕捉球當中的東西
+        Collider[] 範圍內的碰撞器陣列 = Physics.OverlapSphere(this.transform.position, DetectRadius, GroundLayer);
+        // 如果碰撞器碰到的東西的數量不是零表示在地上
+        if (範圍內的碰撞器陣列.Length > 0)
+        {
+            Grounded = true;
+        }
+        else
+        {
+            Grounded = false;
+        }
+    }
+
 
     // Start is called before the first frame update
     void Start()
