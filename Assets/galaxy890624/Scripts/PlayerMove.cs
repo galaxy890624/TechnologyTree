7601f5141dbd4107b672f2d7e97e1c41bd3963ab
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-09-11 15:50:58 +0800

    Update PlayerMove.cs

---
+++ Assets/galaxy890624/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] Rigidbody Physics = null; // player position
+    [SerializeField] Animator Animator = null;
+
+    float X = 0f; // ad
+    float Y = 0f; // space
+    float Z = 0f; // ws
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
