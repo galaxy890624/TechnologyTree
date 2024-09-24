33651ab382cc847631fd302fa4174c42030189f8
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-09-24 08:02:04 +0800

    Update LvUI.cs

---
+++ Assets/galaxy890624/Scripts/LvUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LvUI : MonoBehaviour
+{
+    public TextMeshProUGUI LvText;
+    public TextMeshProUGUI ExpText;
+    public Data Data; // ScriptableObject
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LvText.text = " Lv = " + Data.Level.ToString("N0");
+        ExpText.text = " Exp = " + Data.Exp.ToString("N0") + " / " + Data.MaxExp.ToString("N0");
+    }
+}
