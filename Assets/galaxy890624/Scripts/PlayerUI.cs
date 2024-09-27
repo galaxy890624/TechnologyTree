eaba94f00af6e32cd2b1131efeb77284780d4fb0
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-09-28 02:19:58 +0800

    Update PlayerUI.cs

---
+++ Assets/galaxy890624/Scripts/PlayerUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using StarterAssets;
+using Unity.VisualScripting;
+
+public class PlayerUI : MonoBehaviour
+{
+    public TextMeshProUGUI SpdText; // 玩家的速度
+
+    [SerializeField, Header("玩家物件")]
+    GameObject Player;
+    public ThirdPersonController ThirdPersonController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpdText.text = Player.GetComponent<ThirdPersonController>().MoveSpeed.ToString("N0");
+    }
+}
