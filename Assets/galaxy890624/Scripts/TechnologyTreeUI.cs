7684deff910338d424af19d37f52e7c38b202dad
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-10-02 06:22:03 +0800

    Update TechnologyTreeUI.cs

---
+++ Assets/galaxy890624/Scripts/TechnologyTreeUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TechnologyTreeUI : Windows<TechnologyTreeUI>
+{
+    protected override void Update()
+    {
+        base.Update(); // 先讓Windows底層做事
+        if (Input.GetKeyDown(KeyCode.Tab)) // Press "Tab" key to open "TechnologyTreeUI"
+        {
+            if (isOpen)
+            {
+                Close();
+            }
+            else
+            {
+                Open();
+            }
+        }
+
+    }
+}
