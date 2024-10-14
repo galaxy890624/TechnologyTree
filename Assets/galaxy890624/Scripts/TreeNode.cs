a0fd8ee9115fd0dbd7074f1f3d34a14439f5a027
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-10-15 02:02:36 +0800

    Update TreeNode.cs

---
+++ Assets/galaxy890624/Scripts/TreeNode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeNode : MonoBehaviour
+{
+    struct  SkillTreeNode
+    {
+        int SkillIndex;
+        string SkillName;
+        // SkillTreeNode* next;
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
