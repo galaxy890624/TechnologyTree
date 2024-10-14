44abea09b23788baf65adc32c9bc599e4f8d375c
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-10-14 14:56:02 +0800

    Update SkillData.cs

--- Assets/galaxy890624/Skills/SkillData.cs
+++ Assets/galaxy890624/Skills/SkillData.cs
@@ -14,8 +14,12 @@ public class SkillData : ScriptableObject
     public string SkillName;
     [Header("技能等級"), Range(0, 500)]
     public int Lv = 0;
-    [Header("技能每個等級能力值")]
+    [Header("技能每個等級的能力值")]
     public float[] SkillValues = null;
+    [Header("技能每個等級的所需等級")]
+    public int[] RequireLevels = null;
+    [Header("技能每個等級的所需經驗值")]
+    public int[] RequireExps = null;
 
     // Lambda 運算子 (簡寫)
     // Lambda 符號後可以運算或寫程式
@@ -23,6 +27,8 @@ public class SkillData : ScriptableObject
     /// <summary>
     /// 目前技能的能力值
     /// </summary>
-    public float SkillValue => SkillValues[Lv - 1];
+    public float SkillValue => SkillValues[Lv - 1]; // 注意SkillValue 和 SkillValues[] 的差別
+    public float RequireLevel => RequireLevels[Lv - 1];
+    public float RequireExp => RequireExps[Lv - 1];
 
 }
