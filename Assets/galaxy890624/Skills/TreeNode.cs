6bb10a2f04ac5566453f5f72a2dd8b92d1271c39
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-11-10 02:34:02 +0800

    Update TreeNode.cs

---
+++ Assets/galaxy890624/Skills/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 1.技能解鎖
+/// 2.
+/// </summary>
+[CreateAssetMenu(fileName = "TreeNode", menuName = "Skill Data/new TreeNode")]
+public class TreeNode : ScriptableObject
+{
+    public int SkillIndex;
+    public SkillData SkillData;
+    public SkillData[] UnlockSkill;
+}
