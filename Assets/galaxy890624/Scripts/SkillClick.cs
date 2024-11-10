5d147117848deb188e617733dc684c3e8566e46d
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-11-11 05:30:56 +0800

    Update SkillClick.cs

--- Assets/galaxy890624/Scripts/SkillClick.cs
+++ Assets/galaxy890624/Scripts/SkillClick.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// 點擊技能圖示後 會發生的事情
+/// 1.滑鼠移到技能圖示 顯示技能說明的框框
+/// 2.點擊技能圖示後 會發生的事情
 /// </summary>
 public class SkillClick : MonoBehaviour
 {
-    SkillData SkillData;
-    GameObject SkillImage;
-    GameObject SkillMessageBox; // 技能說明的框框
-    Transform SkillMessageBoxPosition; // 技能說明的框框位置
+    public SkillData SkillData;
+    public GameObject SkillImage; // 技能圖片
+    public Transform SkillImagePosition; // 技能圖片 的位置
+    public GameObject SkillMessageBox; // 技能說明 的 框框
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     private void OnMouseEnter()
     {
-        Instantiate(SkillMessageBox);
+        // 生成在技能圖示 右邊100px
+        // Instantiate( GameObject.name<GameObject>, Vector3, Quaternion.identity, Transform )
+        Instantiate(SkillMessageBox, new Vector3(100f, 0f, 0f), Quaternion.identity, SkillImagePosition);
     }
     private void OnMouseExit()
     {
-        
+        Destroy(SkillMessageBox);
     }
 
     // Update is called once per frame
