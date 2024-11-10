f713c6498615d46eb9b80093b6a66cbf38d9baf8
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-11-10 23:53:59 +0800

    Update SkillClick.cs

--- Assets/galaxy890624/Scripts/SkillClick.cs
+++ Assets/galaxy890624/Scripts/SkillClick.cs
@@ -8,11 +8,17 @@ public class SkillClick : MonoBehaviour
 {
     SkillData SkillData;
     GameObject SkillImage;
+    GameObject SkillMessageBox; // 技能說明的框框
+    Transform SkillMessageBoxPosition; // 技能說明的框框位置
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+    private void OnMouseEnter()
+    {
+        Instantiate(SkillMessageBox);
     }
     private void OnMouseExit()
     {
---
+++ Assets/galaxy890624/Skills/UpgradeSkill.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace galaxy890624
+{
+    /// <summary>
+    /// Upgrade Skill System
+    /// </summary>
+    public class UpgradeSkill : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
