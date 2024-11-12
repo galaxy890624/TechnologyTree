221e58e06ba1b72ca5aa97d03275a30a4f10f8f6
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-11-12 20:12:56 +0800

    Update SkillClick.cs

--- Assets/galaxy890624/Scripts/SkillClick.cs
+++ Assets/galaxy890624/Scripts/SkillClick.cs
@@ -13,13 +13,14 @@ public class SkillClick : MonoBehaviour //, IPointerEnterHandler // IPointerEnte
 {
     public SkillData SkillData;
     public GameObject SkillImage; // 技能圖片
+    public Transform Content;
     public Transform SkillImagePosition; // 技能圖片 的位置
     public GameObject SkillMessageBox; // 技能說明 的 框框
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //SkillImagePosition.position = new Vector3(0f,0f, 0f);
     }
     /// <summary>
     /// 顯示技能說明的框框
@@ -27,22 +28,9 @@ public class SkillClick : MonoBehaviour //, IPointerEnterHandler // IPointerEnte
     public void ShowSkillMessageBox()
     {
         print($"<color=#ff00ff>產生<color=#00ff00>{SkillMessageBox.name}</color>!</color>");
-        Instantiate(SkillMessageBox, new Vector3(100f, 0f, 0f), Quaternion.identity, SkillImagePosition); // 產生的位置不對
-    }
-    /*private void OnMouseEnter()
-    {
-        // 捕捉滑鼠的圖片
-        print($"<color=#ff00ff>滑鼠在<color=#00ff00>{gameObject.name}</color>!</color>");
-        // SkillImage = this.gameObject;
-
         // 生成在技能圖示 右邊100px
-        // Instantiate( GameObject.name<GameObject>, Vector3, Quaternion.identity, Transform )
-        Instantiate(SkillMessageBox, new Vector3(100f, 0f, 0f), Quaternion.identity, SkillImagePosition);
+        Instantiate(SkillMessageBox, new Vector3(100f, 0f, 0f), Quaternion.identity, Content); // 產生的位置不對
     }
-    private void OnMouseExit()
-    {
-        // Destroy(SkillMessageBox);
-    }*/
 
     // Update is called once per frame
     void Update()
