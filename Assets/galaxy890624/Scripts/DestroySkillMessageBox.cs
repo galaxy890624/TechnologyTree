41ba1185fb0229d5712294c348fbe64a5f263af8
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2024-11-13 22:25:27 +0800

    Update UpgradeSkill.cs

--- Assets/galaxy890624/Scripts/DestroySkillMessageBox.cs
+++ Assets/galaxy890624/Scripts/DestroySkillMessageBox.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// 注意 Instantiate 和 Destroy 不可以寫在同一個.cs腳本裡, 否則會衝突
+/// </summary>
 public class DestroySkillMessageBox : MonoBehaviour
 {
     public GameObject SkillMessageBox;
--- Assets/galaxy890624/Skills/UpgradeSkill.cs
+++ Assets/galaxy890624/Skills/UpgradeSkill.cs
@@ -11,7 +11,7 @@ namespace galaxy890624
         // Start is called before the first frame update
         void Start()
         {
-        
+            
         }
 
         // Update is called once per frame
