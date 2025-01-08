464c1aebfa17be13fc4a0f44ca853a7ba30cf7d4
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2025-01-08 13:13:02 +0800

    Update 20250108

--- Assets/galaxy890624/Skills/SkillData.cs
+++ Assets/galaxy890624/Skills/SkillData.cs
@@ -9,29 +9,22 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "SkillData", menuName = "Skill Data/new Skill Data")]
 public class SkillData : ScriptableObject
 {
+    // 技能基本資料
     [Header("技能索引值")]
     public int SkillIndex;
+    [Header("技能的顯示圖片")]
+    Sprite SkillSprite;
     [Header("技能名稱")]
     public string SkillName;
     [Header("技能等級"), Range(0, 500)]
-    public int Lv = 0;
-    [Header("技能每個等級的能力值")]
-    public float[] SkillValues = null;
-    [Header("技能每個等級的所需等級")]
-    public int[] RequireLevels = null;
-    [Header("技能每個等級的所需經驗值")]
-    public float[] RequireExps = null;
-    [Header("技能每個等級的描述")]
+    public int SkillLevel = 0;
+    [Header("技能的能力值")]
+    public float SkillValue = 0;
+    [Header("技能的描述")]
     [TextArea] // 可換行
-    public string[] SkillInfos;
-    // Lambda 運算子 (簡寫)
-    // Lambda 符號後可以運算或寫程式
-
-    /// <summary>
-    /// 目前技能的能力值
-    /// </summary>
-    public float SkillValue => SkillValues[Lv]; // 注意SkillValue 和 SkillValues[] 的差別
-    public int RequireLevel => RequireLevels[Lv];
-    public float RequireExp => RequireExps[Lv];
-    public string SkillInfo => SkillInfos[Lv];
+    public string SkillInfo;
+    [Header("是否被解鎖")]
+    public bool IsUnlocked;
+    [Header("解鎖的條件")]
+    public SkillData[] PreSkills;
 }
---
+++ Assets/galaxy890624/Skills/SkillManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace galaxy890624
+{
+    /// <summary>
+    /// 所有的技能列表
+    /// </summary>
+    public class SkillManager : MonoBehaviour
+    {
+        public static SkillManager Instance; // 宣告單例模式
+        [Header("點擊到的技能按鈕")]
+        public SkillData ActivateSkill;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                if (Instance != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
+
