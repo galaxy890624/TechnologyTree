7a883540e4ad3117427da54c5ca7b0051aaf65a5
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2025-01-12 17:31:00 +0800

    Update SkillManager.cs

--- Assets/galaxy890624/Skills/SkillManager.cs
+++ Assets/galaxy890624/Skills/SkillManager.cs
@@ -90,7 +90,7 @@ namespace galaxy890624
         // Start is called before the first frame update
         void Start()
         {
-        
+
         }
 
         // Update is called once per frame
