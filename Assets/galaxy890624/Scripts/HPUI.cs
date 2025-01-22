72c5db6ad2d80959f1dfd94df534d50c766a19de
galaxy890624 <99355301+galaxy890624@users.noreply.github.com>
2025-01-23 02:23:28 +0800

    Update HPUI.cs

---
+++ Assets/galaxy890624/Scripts/HPUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace galaxy890624
+{
+    /// <summary>
+    /// 血量顯示
+    /// </summary>
+    public class HPUI : MonoBehaviour
+    {
+        // 當前血量
+        public Image CurrentHPImage;
+        // 受傷血量變化
+        public Image HurtedHPImage;
+
+        /// <summary>
+        /// 血量變化
+        /// </summary>
+        /// <param name="Rate">Rate = Current / Max> </param>
+        public void OnHPChanged(float Rate)
+        {
+            CurrentHPImage.fillAmount = Rate;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
